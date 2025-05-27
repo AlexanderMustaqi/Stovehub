d4fa9e2702eeafc5f425b48cfc3c3082707ca8b6
ARKOUDAKII <65489567+ARKOUDAKII@users.noreply.github.com>
2025-05-27 15:35:22 +0300

    git push

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     const [posts, setPost] = useState(["","","","","","","","","","","","","","","","","","","","","","","","","","","","",]);
 
     return(<>
-            <h1>hello</h1>
+            <h1>hello i am gay</h1>
             <ul className='homepage'>
                 {posts.map((post, index) => <li key={index}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem placeat enim, laudantium facere repellat quas odio tempore distinctio voluptatibus sunt blanditiis beatae. Optio eaque deserunt inventore culpa itaque doloribus.</li>)}
             </ul>
